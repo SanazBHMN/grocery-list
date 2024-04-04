61b990b7d3b60e98b742d9c3b17b4941d313cdea
Sanaz Bahmani <sanazbahmani35@gmail.com>
2024-04-04 17:55:43 +0330

    Build the structure of the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
 import "./App.css";
+import groceryCartImg from "./assets/grocery-cart.jpg";
 
 function App() {
   return (
-    <div className="app">
-      <h1>Hello</h1>
-    </div>
+    <main className="app">
+      <div>
+        <div>
+          <h4 className="success">You're Done</h4>
+          <div className="header">
+            <h1>Shopping List</h1>
+            <img src={groceryCartImg} alt="grocery cart" />
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Add an item"
+              className="item-input"
+            />
+          </div>
+        </div>
+        <ul>
+          <li>
+            <div className="container">
+              <input type="checkbox" name="" id="" />
+              <p>Carrots</p>
+            </div>
+            <div>
+              <button className="remove-button">X</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </main>
   );
 }
 
